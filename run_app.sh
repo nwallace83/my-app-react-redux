1a1a6c7f571f7c02ff1ac35a80d2358e7fbd9e90
Nathan Wallace <nwallace6988@gmail.com>
2021-07-04 20:00:33 -0600

    Run docker container as daemon

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+git pull
+npm install
+npm run build
 docker rm -f my-web-app
 docker build --rm -f Dockerfile.prod -t my-web-app .
-docker run -it --rm --name my-web-app -p 1337:80 my-web-app
+docker run -d --restart=always --name my-web-app -p 1337:80 my-web-app
