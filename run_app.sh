2d18b00f1eea4b812e0a297f0e538baa29041cfc
Nathan Wallace <nwallace6988@gmail.com>
2021-07-09 22:11:35 -0600

    add hostname to docker container

--- run_app.sh
+++ run_app.sh
@@ -1,7 +1,8 @@
 #/bin/bash
+git checkout package-lock.json
 git pull
 npm install
 npm run build
 docker rm -f my-web-app
 docker build --rm -f Dockerfile.prod -t my-web-app .
-docker run -d --restart=always --name my-web-app -p 1337:80 my-web-app
+docker run -d --network my-net --network web1 --restart=always --name my-web-app -p 1337:80 my-web-app
