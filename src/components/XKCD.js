a759dbfd7729ef511999cf642659978b156601b2
Nathan Wallace <nwallace6988@gmail.com>
2021-07-09 23:17:59 -0600

    Add spinner to comic refreshing

--- src/components/XKCD.js
+++ src/components/XKCD.js
@@ -5,10 +5,11 @@ import { addComics } from '../reducers/XKCDSlice';
 import { deleteAllComics } from '../reducers/XKCDSlice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSync } from '@fortawesome/free-solid-svg-icons'
+import Spinner from 'react-bootstrap/Spinner'
 
 const mapStateToProps = (state) => {
     return {
-        comics: state.XKCD.comics
+        comics: state.XKCD.comics,
     }
 }
 
@@ -22,17 +23,19 @@ class XKCD extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            comic: []
+            isLoading: false,
         }
     }
 
     componentDidMount = () => {
         if (this.props.comics.length <= 0) {
+            this.setState({isLoading: true})
             this.getComics()
         }
     }
 
     refreshComics = () => {
+        this.setState({isLoading: true})
         this.props.dispatch(deleteAllComics())
         this.getComics()
     }
@@ -41,18 +44,37 @@ class XKCD extends React.Component {
         fetch('/api/xkcd')
             .then(res => res.json())
             .then(res => this.props.dispatch(addComics(res)))
+            .finally(res => this.setState({isLoading: false}))
     }
 
     render() {
-        return ( 
+        return (
             <div id="comic-div" className="row margin-top-5">
                 <div className="margin-all-5 col-lg-12">
-                    <button className="btn btn-dark" onClick={this.refreshComics}>
-                        <FontAwesomeIcon icon={faSync} id="xkcd-refresh"/>Get New Comics
-                    </button>
+                    <RefreshButton onClick={this.refreshComics} isLoading={this.state.isLoading}/>
                 </div>
-                {this.props.comics.map( (comic,index) => { return <Comic comic={comic} key={index} index={index} /> })} 
+                {this.props.comics.map( (comic,index) => { return <Comic comic={comic} key={index} index={index} /> })}
             </div>
+        )
+    }
+}
+
+class RefreshButton extends React.Component {
+    getSpinnerIcon = () => {
+        switch (this.props.isLoading) {
+            case true: return <Spinner animation="border" role="status" id="xkcd-refresh">
+                                <span className="sr-only">Loading...</span>
+                            </Spinner>
+            default: return <FontAwesomeIcon icon={faSync} id="xkcd-refresh"/>
+        } 
+    }
+
+    render() {
+        return (
+            <button className="btn btn-dark" onClick={this.props.onClick}>
+                {this.getSpinnerIcon()}
+                <span>Get New Comics</span>
+            </button>
         )
     }
 }
