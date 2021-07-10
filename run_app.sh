ad5d82a26903934b3e84095fdb2f2617b8c6dbbc
Nathan Wallace <nwallace6988@gmail.com>
2021-07-09 22:13:31 -0600

    fix flag in docker script

--- run_app.sh
+++ run_app.sh
@@ -5,4 +5,4 @@ npm install
 npm run build
 docker rm -f my-web-app
 docker build --rm -f Dockerfile.prod -t my-web-app .
-docker run -d --network my-net --network web1 --restart=always --name my-web-app -p 1337:80 my-web-app
+docker run -d --network my-net --hostname web1 --restart=always --name my-web-app -p 1337:80 my-web-app
