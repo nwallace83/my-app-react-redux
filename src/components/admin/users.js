8d3db57abefb2ece14cca041536d9c0fd7fc7dd8
Nathan Wallace <nwallace6988@gmail.com>
2021-07-28 20:40:32 -0600

    Add user deletion functionality

--- src/components/admin/users.js
+++ src/components/admin/users.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Table } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class Users extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            users:[]
+            users:[],
+            deleteUserDialogue: false,
+            userToDelete: null,
         }
     }
 
@@ -17,6 +25,26 @@ class Users extends React.Component {
 
     componentDidMount() {
         this.fetchUsers()
+    }
+
+    handleClose() {
+        this.setState({deleteUserDialogue: false, userToDelete: null})
+    }
+
+    confirmDeleteUser(userName) {
+        this.setState({deleteUserDialogue: true, userToDelete: userName})
+    }
+
+    deleteUser(userName) {
+        fetch('/admin/users/'+userName, {method: "DELETE"})
+        .then(() => {
+            if (Response.ok) {
+                this.setState({users: this.state.users.filter(user => {
+                    return user.userName !== userName;
+                })})
+            }
+            this.handleClose();
+        }).catch(err => console.log(err))
     }
 
     render() {
@@ -32,6 +60,7 @@ class Users extends React.Component {
                             <tr>
                                 <th>Username</th>
                                 <th>Role</th>
+                                <th className="txt-center">Delete</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -40,14 +69,38 @@ class Users extends React.Component {
                             <tr key={index}>
                             <td>{user.userName}</td>
                             <td>{user.role}</td>
+                            <td className="txt-center"><FontAwesomeIcon icon={faTrashAlt} onClick={() => this.confirmDeleteUser(user.userName)}/></td>
                             </tr>
                         )
                         })}
                         </tbody>
                     </Table>
                 </div>
+                <div>
+                    {this.deleteUserConfirmation()}
+                </div>
             </div>
         )
+    }
+
+    deleteUserConfirmation() {
+        if (this.state.deleteUserDialogue) {
+            return (
+                <Dialog open={this.state.deleteUserDialogue} onClose={this.handleClose}>
+                <DialogTitle>{"Are you sure you want to delete " + this.state.userToDelete + "?"}</DialogTitle>
+                <DialogActions>
+                <Button onClick={() => this.deleteUser(this.state.userToDelete)} color="primary">
+                    YES
+                </Button>
+                <Button onClick={() => this.handleClose()} color="primary" autoFocus>
+                    NO
+                </Button>
+                </DialogActions>
+            </Dialog>
+            )
+        } else {
+            return null;
+        }
     }
 }
 
